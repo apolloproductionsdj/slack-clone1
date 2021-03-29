e3af0356f54d1bf338acece48a3b1e9c413f5db6
mike <apolloproductionsdj@gmail.com>
2021-03-28 23:41:06 -0700

    fixed addChannelOption error by correcting misspelling

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function SidebarOption({ Icon, title, addchannelOption }) {
+function SidebarOption({ Icon, title, addChannelOption }) {
 
     const addChannel = () => {
 
